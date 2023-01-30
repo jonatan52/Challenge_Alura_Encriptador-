8c61af8f6ccbd2c7087c47df5331fb646ed389ea
jonatan52 <tarriba52@gmail.com>
2023-01-30 04:55:40 -0500

    Agregando codigo javascript

--- Encriptador/js/encriptador.js
+++ Encriptador/js/encriptador.js
@@ -1,4 +1,4 @@
-// Las "llaves" de encriptación que utilizaremos son las siguientes:
+        // Las "llaves" de encriptación que utilizaremos son las siguientes:
 
 // La letra "e" es convertida para "enter"
 // La letra "i" es convertida para "imes"
@@ -16,18 +16,24 @@
 // "gato" => "gaitober"
 // gaitober" => "gato"
 
-let letra = "e" ; "enter"
-let letra = "i" ; "imes"
-let letra = "a" ; "ai"
-let letra = "o" ; "ober"
-let letra = "u" ; "ufat"
+// let letra = "e" ; "enter"
+// let letra = "i" ; "imes"
+// let letra = "a" ; "ai"
+// let letra = "o" ; "ober"
+// let letra = "u" ; "ufat"
 
-                  "enter"
-let letra = "i" ; "imes"
-let letra = "a" ; "ai"
-let letra = "o" ; "ober"
-let letra = "u" ; "ufat"
+//                   "enter"
+// let letra = "i" ; "imes"
+// let letra = "a" ; "ai"
+// let letra = "o" ; "ober"
+// let letra = "u" ; "ufat"
 
 function encriptar(){
-    let texto = document.getElementsByClassName("encriptar").value.toLowerCase();
+    let texto = document.getElementById("texArea").value.toLowerCase();
 }
+
+var texArea = texto.replace(/e/img, "enter");
+var texArea = texto.replace(/0/img, "ober");   
+var texArea = texto.replace(/i/img, "imes");
+var texArea = texto.replace(/a/img, "ai");
+var texArea = texto.replace(/u/img, "ufat");
