f3ccec45a2c4e93cd6c46f781b1475a9f7e2ed72
Jonatan <tarriba52@gmail.com>
2023-01-29 19:09:20 -0500

    actualizacion de archivos

---
+++ Encriptador/js/encriptador.js
@@ -0,0 +1,33 @@
+// Las "llaves" de encriptación que utilizaremos son las siguientes:
+
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+
+// Requisitos:
+// - Debe funcionar solo con letras minúsculas
+// - No deben ser utilizados letras con acentos ni caracteres especiales
+// - Debe ser posible convertir una palabra para la versión encriptada también devolver una 
+//   palabra encriptada para su versión original.
+
+// Por ejemplo:
+// "gato" => "gaitober"
+// gaitober" => "gato"
+
+let letra = "e" ; "enter"
+let letra = "i" ; "imes"
+let letra = "a" ; "ai"
+let letra = "o" ; "ober"
+let letra = "u" ; "ufat"
+
+                  "enter"
+let letra = "i" ; "imes"
+let letra = "a" ; "ai"
+let letra = "o" ; "ober"
+let letra = "u" ; "ufat"
+
+function encriptar(){
+    let texto = document.getElementsByClassName("encriptar").value.toLowerCase();
+}
