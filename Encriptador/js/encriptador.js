090b4ee3a1434d0244655fcedb8bf34b177971a8
jonatan52 <tarriba52@gmail.com>
2023-02-03 23:42:38 -0500

    Actualizacion del HTML, CSS y JS

--- Encriptador/js/encriptador.js
+++ Encriptador/js/encriptador.js
@@ -18,35 +18,36 @@
 
 /*-----------Funcion encriptar---------------*/
 function encriptar() {
-    var texto = document.getElementById("textArea").value.toLowerCase();
+    let texto = document.getElementById("textArea").value.toLowerCase();
     element = document.getElementById("imagen");
     element.src = src = "img/haker.jpg";
 
-    var texArea = texto.replace(/e/img, "enter");
-    var texArea = texArea.replace(/o/img, "ober");
-    var texArea = texArea.replace(/i/img, "imes");
-    var texArea = texArea.replace(/a/img, "ai");
-    var texArea = texArea.replace(/u/img, "ufat");
+     texArea = texto.replace(/e/img, "enter");
+     texArea = texArea.replace(/o/img, "ober");
+     texArea = texArea.replace(/i/img, "imes");
+     texArea = texArea.replace(/a/img, "ai");
+     texArea = texArea.replace(/u/img, "ufat");
 
 
     document.getElementById("textoDesencriptado").innerHTML = texArea;
     document.getElementById("boton-copiar").style.display = "show";
     document.getElementById("boton-copiar").style.display = "inherit";
     document.getElementById("h3-encontrado").style.display = "none";
+  
 
 
 }
 /*-----------Funcion Desencriptar---------------*/
 function desencriptar() {
-    var texto = document.getElementById("textArea").value.toLowerCase();
+    let texto = document.getElementById("textArea").value.toLowerCase();
     element = document.getElementById("imagen");
     element.src = src = "img/hacker-2.jpg";
 
-    var texArea = texto.replace(/enter/img, "e");
-    var texArea = texArea.replace(/ober/img, "o");
-    var texArea = texArea.replace(/imes/img, "i");
-    var texArea = texArea.replace(/ai/img, "a");
-    var texArea = texArea.replace(/ufat/img, "u");
+     texArea = texto.replace(/enter/img, "e");
+     texArea = texArea.replace(/ober/img, "o");
+     texArea = texArea.replace(/imes/img, "i");
+     texArea = texArea.replace(/ai/img, "a");
+     texArea = texArea.replace(/ufat/img, "u");
 
 
     document.getElementById("textoDesencriptado").innerHTML = texArea;
@@ -57,7 +58,7 @@ function desencriptar() {
 
 /*-----------Funcion Copiar---------------*/
 function copy() {
-    var contenido = document.querySelector("#textoDesencriptado");
+    let contenido = document.querySelector("#textoDesencriptado");
     contenido.select();
     document.execCommand("copy");
     alert("Texto copiado");
@@ -66,9 +67,10 @@ function copy() {
 /*-----------Funcion borrar ---------------*/
 
 function borrarTexto() {
-    var borrar = document.getElementById("textArea", "textoDesencriptado").value = "";
+    let borrar = document.getElementById("textArea", "textoDesencriptado").value = "";
     borrar = document.getElementById("textoDesencriptado").value = "";
     borrar = document.getElementById("h3-encontrado").value = "show";
+    document.location.reload();
 
 
 }
